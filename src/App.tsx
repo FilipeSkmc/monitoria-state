d78de22055dea67a29ae4a1f5fc23c26cd6fc1a9
FilipeSkmc <filipe-santana@outlook.com>
2023-08-22 11:06:12 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,19 @@
-import { useState } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
 import Musics from './components/Musics';
 
 import './App.css';
 
-import { MusicType, musics } from './data/data';
+import { musics } from './data/data';
 
 function App() {
-  // criando um state para mostrar ou não o form
-  const [showForm, setShowForm] = useState<boolean>(false);
-
-  // criando um state para a lista de músicas
-  // o valor inicial é o array de músicas que está no arquivo data.ts
-  const [musicList, setMusicList] = useState<MusicType[]>(musics);
-
-  // função para adicionar a música na lista
-  const handleAddMusic = (music: MusicType) => {
-    // adicionando a música no array de músicas
-    // o spread operator (...) pega todos os itens do array e adiciona no novo array
-    setMusicList([...musicList, music]);
-  };
-
   return (
     <div className="app">
       <Header />
 
-      {/*
-        se showForm for true, renderiza o form
-        se showForm for false, renderiza o botão para mostrar o form
-      */}
+      <Form />
 
-      { !showForm ? (
-        <button
-          onClick={ () => setShowForm(true) }
-        >
-          Nova Música
-        </button>
-      ) : (
-        <Form setShowForm={ setShowForm } handleAddMusic={ handleAddMusic } />
-      ) }
-
-      <Musics musics={ musicList } />
+      <Musics musics={ musics } />
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,56 +1,19 @@
-import { useState } from 'react';
-import { MusicType } from '../data/data';
+import React from 'react';
 import './Form.css';
 
-// criando um type para o props
-type FormProps = {
-  setShowForm: (show: boolean) => void;
-  handleAddMusic: (music: MusicType) => void;
-};
-
-function Form({ setShowForm, handleAddMusic }: FormProps) {
-  const [formData, setFormData] = useState<MusicType>({
-    name: '',
-    duration: '',
-  });
-  // descontruindo o state para usar de forma mais simples
-  const { name, duration } = formData;
-
-  // validações
-  const nameOk = name.length < 2;
-  const durationOk = (/^[0-9]+:[0-9]{2}$/).test(duration);
-
-  // desabilitando o botão de adicionar
-  const disabled = nameOk || !durationOk;
-
-  // função para mudar o state
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // descontruindo o event para usar de forma mais simples
-    // nameInput é o name do input, mudei o nome para não confundir com o name do state
-    const { name: nameInput, value } = e.target;
-    setFormData({
-      ...formData,
-      [nameInput]: value,
-    });
+function Form() {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
   };
 
-  // função para adicionar a música e limpar o form
-  const handleClick = (e: React.FormEvent<HTMLElement>) => {
-    // previnindo o comportamento padrão do form
-    e.preventDefault();
-    // chamando a função que adiciona a música
-    handleAddMusic(formData);
-    // limpando o form
-    setFormData({
-      name: '',
-      duration: '',
-    });
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log(event.target);
   };
 
   return (
     <div className="form-data">
       <form
-        onSubmit={ handleClick }
+        onSubmit={ handleSubmit }
       >
         <label htmlFor="name">
           Música:
@@ -59,10 +22,8 @@ function Form({ setShowForm, handleAddMusic }: FormProps) {
           type="text"
           id="name"
           name="name"
-          value={ name }
           onChange={ handleChange }
         />
-        { nameOk && <span>No mínimo 2 caractéres</span>}
 
         <label htmlFor="duration">
           Duração:
@@ -71,20 +32,17 @@ function Form({ setShowForm, handleAddMusic }: FormProps) {
           type="text"
           id="duration"
           name="duration"
-          value={ duration }
           onChange={ handleChange }
         />
-        <span>{ !durationOk ? ('Formato válido MM:SS') : 'Nice!' }</span>
 
         <button
-          disabled={ disabled }
+          disabled
         >
           Adicionar
         </button>
 
         <button
           type="button"
-          onClick={ () => setShowForm(false) }
         >
           Cancelar
         </button>
